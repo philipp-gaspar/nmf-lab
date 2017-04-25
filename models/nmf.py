4502fb90faec93ce06a4230225315b8803c8d0da
Philipp Gaspar <philipp.gaspar@cern.ch>
2017-04-25 18:41:44 -0300

    First commit for HALS model

--- models/nmf.py
+++ models/nmf.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 import json
+import time
+
+from utils.matrix_utils import column_norm, scale_factor_matrices
 
 class NMF(object):
     """
@@ -8,16 +11,12 @@ class NMF(object):
     """
 
     def __init__(self):
-        """
-        """
         self.default_max_iter = 100
 
-    def run(self,
-            Y,
-            j,
+    def run(self, Y, j,
             init=None,
             max_iter=self.default_max_iter,
-            verbose=False):
+            verbose=True):
         """
         Run a particular NMF algorithm.
 
@@ -36,7 +35,6 @@ class NMF(object):
         A: factor matrix, shape (i, j)
         B: coefficient matrix, shape (t, j)
         """
-
         self.info = {'j': j,
                      'A_dim_1': A.shape[0],
                      'A_dim_2:' A.shape[1],
@@ -44,11 +42,85 @@ class NMF(object):
                      'max_iter': max_iter,
                      'verbose': verbose}
 
-        # first implement the uniform random itialization
-        self.info['init'] = 'uniform_random'
-        A = np.random.rand(Y.shape[0], j)
-        B = np.random.rand(Y.shape[1], j)
+        # Initialization of factor matrices
+        if init != None:
+            A = init[0].copy()
+            B = init[1].copy()
+            self.info['init'] = 'user_provided'
+        else:
+            A = np.random.rand(Y.shape[0], j)
+            B = np.random.rand(Y.shape[1], j)
+            self.info['init'] = 'uniform_random'
 
         if verbose:
             print "[NMF] Running: "
             print json.dumps(self.info, indent=4, sort_keys=True)
+
+        start = time.time()
+        # Algorithm specific initialization
+        A, B = self.initializer(A, B)
+
+        # Iteration Process
+        for i in range(1, self.info['max_iter'] + 1):
+            A, B = self.iter_solver(Y, A, B, j, i)
+
+        # Normalize matrices A and B in order to not
+        # modify the product A.dot(B.T)
+        A, B = scale_factor_matrices(A, B)
+
+        # Final info
+        self.final = {}
+        self.final['iterations'] = i
+        self.final['elapsed'] = time.time() - start
+        if verbose:
+            print "[NMF] Completed:"
+            print json.dumps(self.final, indent=4, sort_keys=True)
+
+        return A, B
+
+    def iter_solver(self, Y, A, B, j, it):
+        raise NotImplementedError
+
+    def initializer(self, A, B):
+        return A, B
+
+class NMF_HALS(NMF):
+    """
+    NMF algorithm: Hierarchical Alternating Least Squares
+
+    Improved and modified HALS NMF algorithm called:
+    FAST HALS for Large Scale NMF
+    """
+
+    def __init__(self, default_max_iter=100):
+        self.eps = 1e-16
+
+    def initializer(self, A, B):
+        # Normalize columns of matrix A to l2 unity norm
+        norm_vec = column_norm(A, by_norm='2')
+        A = A / norm_vec[None, :]
+
+        return A, B
+
+    def iter_solver(self, Y, A, B, j, it):
+        """
+        """
+        # Update B
+        W = Y.T.dot(A)
+        V = A.T.dot(A)
+
+        for jj in range(j):
+            b_jj = B[:, jj] + W[:, jj] - B.dot(V[:, jj])
+            B[:, jj] = np.maximum(b_jj, self.eps)
+
+        # Update A
+        P = Y.dot(B)
+        Q = B.T.dot(B)
+
+        for jj in range(j):
+            a_jj = A[:, jj] * Q[jj, jj] + P[:, jj] - A.dot(Q[:, jj])
+            A[:, jj] = np.maximum(a_jj, self.eps)
+            norm_value = np.sqrt(np.sum(A[:, jj] * A[:, jj]))
+            A[:, jj] = A[:, jj] / norm_value
+
+        return A, B
