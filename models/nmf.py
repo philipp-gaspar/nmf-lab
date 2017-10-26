78d507f5254be0c748a8c86fb57f0604c718f085
Philipp Gaspar <philipp.gaspar@cern.ch>
2017-10-26 18:48:51 -0200

    Frobenius with the new notation.

--- models/nmf.py
+++ models/nmf.py
@@ -5,6 +5,7 @@ import json
 import time
 
 from ..utils.matrix_utils import column_norm, scale_factor_matrices
+from ..utils.metrics import frobenius_norm
 
 class NMF(object):
     """
@@ -187,57 +188,57 @@ class NMF(object):
     def initializer(self, W, H):
         return W, H
 
-class NMF_HALS(NMF):
-    """
-    NMF algorithm: Hierarchical Alternating Least Squares
-
-    Improved and modified HALS NMF algorithm called:
-    FAST HALS for Large Scale NMF
-
-    REF [1]: Pseudo-Code in the book: Nonnegative Matrix and Tensor
-    Factorizations - A. Cichocki et All; Page 219; Algorithm 4.3
-    """
-    def __init__(self, default_max_iter=100):
-        self.eps = 1e-16
-
-    def initializer(self, A, B):
-        # Normalize columns of matrix A to l2 unity norm
-        norm_vec = column_norm(A, by_norm='2')
-        A = A / norm_vec[None, :]
-
-        return A, B
-
-    def iter_solver(self, Y, A, B, j, it):
-        # Check the cost function. FAST HALS algorithm only
-        # accepts Frobenius.
-        if self.cost_function != 'frobenius':
-            print "Cost function not valid for HALS algorithm."
-            print "Try: 'frobenius'."
-            sys.exit()
-
-        # Update B
-        W = Y.T.dot(A)
-        V = A.T.dot(A)
-
-        for jj in range(j):
-            b_jj = B[:, jj] + W[:, jj] - B.dot(V[:, jj])
-            B[:, jj] = np.maximum(b_jj, self.eps)
-
-        # Update A
-        P = Y.dot(B)
-        Q = B.T.dot(B)
-
-        for jj in range(j):
-            a_jj = A[:, jj] * Q[jj, jj] + P[:, jj] - A.dot(Q[:, jj])
-            A[:, jj] = np.maximum(a_jj, self.eps)
-            norm_value = np.sqrt(np.sum(A[:, jj] * A[:, jj]))
-            A[:, jj] = A[:, jj] / norm_value
-
-        # Calculate error
-        Y_hat = A.dot(B.T)
-        error = 0.5 * np.linalg.norm(Y-Y_hat, ord='fro')
-
-        return A, B, error
+# class NMF_HALS(NMF):
+#     """
+#     NMF algorithm: Hierarchical Alternating Least Squares
+#
+#     Improved and modified HALS NMF algorithm called:
+#     FAST HALS for Large Scale NMF
+#
+#     REF [1]: Pseudo-Code in the book: Nonnegative Matrix and Tensor
+#     Factorizations - A. Cichocki et All; Page 219; Algorithm 4.3
+#     """
+#     def __init__(self, default_max_iter=100):
+#         self.eps = 1e-16
+#
+#     def initializer(self, A, B):
+#         # Normalize columns of matrix A to l2 unity norm
+#         norm_vec = column_norm(A, by_norm='2')
+#         A = A / norm_vec[None, :]
+#
+#         return A, B
+#
+#     def iter_solver(self, Y, A, B, j, it):
+#         # Check the cost function. FAST HALS algorithm only
+#         # accepts Frobenius.
+#         if self.cost_function != 'frobenius':
+#             print "Cost function not valid for HALS algorithm."
+#             print "Try: 'frobenius'."
+#             sys.exit()
+#
+#         # Update B
+#         W = Y.T.dot(A)
+#         V = A.T.dot(A)
+#
+#         for jj in range(j):
+#             b_jj = B[:, jj] + W[:, jj] - B.dot(V[:, jj])
+#             B[:, jj] = np.maximum(b_jj, self.eps)
+#
+#         # Update A
+#         P = Y.dot(B)
+#         Q = B.T.dot(B)
+#
+#         for jj in range(j):
+#             a_jj = A[:, jj] * Q[jj, jj] + P[:, jj] - A.dot(Q[:, jj])
+#             A[:, jj] = np.maximum(a_jj, self.eps)
+#             norm_value = np.sqrt(np.sum(A[:, jj] * A[:, jj]))
+#             A[:, jj] = A[:, jj] / norm_value
+#
+#         # Calculate error
+#         Y_hat = A.dot(B.T)
+#         error = 0.5 * np.linalg.norm(Y-Y_hat, ord='fro')
+#
+#         return A, B, error
 
 class NMF_MU(NMF):
     """
@@ -254,35 +255,33 @@ class NMF_MU(NMF):
     def __init__(self, default_max_iter=100):
         self.eps = 1e-16
 
-    def initializer(self, A, B):
+    def initializer(self, W, H):
         # Normalize columns of matrix A to l1 unity norm
-        norm_vec = column_norm(A, by_norm='1')
-        A = A / norm_vec[None, :]
+        norm_vec = column_norm(W, by_norm='1')
+        W = W / norm_vec[None, :]
 
-        return A, B
+        return W, H
 
     def iter_solver(self, Y, A, B, j, it):
         # FROBENIUS
         if self.cost_function == 'frobenius':
-            # Update A
-            YB = Y.dot(B)
-            numerator = A * YB
-            denominator = A.dot(B.T.dot(B)) + self.eps
-            A = numerator / denominator
+            # Update W
+            numerator = W * (V.dot(H.T))
+            denominator = W.dot(H.dot(H.T)) + self.eps
+            W = numerator / denominator
 
-            # normalize columns of A
-            norm_vec = column_norm(A, by_norm='1')
-            A = A / norm_vec[None, :]
+            # normalize columns of W
+            norm_vec = column_norm(W, by_norm='1')
+            W = W / norm_vec[None, :]
 
-            # Update B
-            YtA = Y.T.dot(A)
-            numerator = B * YtA
-            denominator = B.dot(A.T.dot(A)) + self.eps
-            B = numerator / denominator
+            # Update H
+            numerator = H * (W.T.dot(V))
+            denominator = W.T.dot(W.dot(H)) + self.eps
+            H = numerator / denominator
 
             # Calculate error
-            Y_hat = A.dot(B.T)
-            error = 0.5 * np.linalg.norm(Y-Y_hat, ord='fro')
+            V_hat = W.dot(H)
+            error = frobenius_norm(V, V_hat)
 
         # KULLBACK-LEIBLER
         elif self.cost_function == 'kullback-leibler':
@@ -347,65 +346,65 @@ class NMF_MU(NMF):
 
         return A, B, error
 
-class LOCAL_NMF(NMF):
-    """
-    Local Non-Negative Matrix Factorization (LNMF)
-
-    This NMF algorithmm is aimed to learn spatially localized, parts-based
-    subspace representations of visual patterns.
-    An objective fuction (Kullback-Leibler) localization constrain, in addition
-    to the non-negativity constrain in the standard NMF.
-
-    REF [1]: Learning Spatially Localized, Parts-Based Representations -
-    S. Z. Li, X. Hou, H. Zhang and Q. Cheng - (2001).
-
-    NOTE
-    ----
-    Even though the stated objective function has weights alpha and
-    beta, these disappear in the simplifications made by the authors when
-    deriving the updates. Therefore, we report the regular Kullback-Leibler
-    divergence as the error here.
-    """
-    def __init__(self, default_max_iter=100):
-        self.eps = 1e-16
-
-    def initializer(self, A, B):
-        # Normalize columns of matrix A to l1 unity norm
-        norm_vec = column_norm(A, by_norm='1')
-        A = A / norm_vec[None, :]
-
-        return A, B
-
-    def iter_solver(self, Y, A, B, j, it):
-        # Check the cost function. LNMF only accepts Kullback-Leibler.
-        if self.cost_function != 'kullback-leibler':
-            print("Cost function not valid for LNMF algorithm.")
-            print("Try: 'kullback-leibler'.")
-            sys.exit()
-
-        X = B.T # to keep with the original form
-        ones = np.ones([Y.shape[0], Y.shape[1]])
-        AX = A.dot(X) + self.eps # initial reconstruction
-
-        # Update A
-        numerator = A * ((Y / AX).dot(X.T))
-        denominator = np.maximum(ones.dot(X.T), self.eps)
-        A = numerator / denominator
-
-        # normalize columns of A
-        norm_vec = column_norm(A, by_norm='1')
-        A = A / norm_vec[None, :]
-
-        # update reconstruction
-        AX = A.dot(X) + self.eps
-
-        # Update B
-        AX = np.maximum(AX, self.eps)
-        X = np.sqrt(X * (A.T.dot(Y / AX)))
-        B = X.T
-
-        # Calculate error
-        Y_hat = A.dot(B.T) + self.eps
-        error = ((Y * np.log((Y/Y_hat) + self.eps)) - Y + Y_hat).sum(axis=None)
-
-        return A, B, error
+# class LOCAL_NMF(NMF):
+#     """
+#     Local Non-Negative Matrix Factorization (LNMF)
+#
+#     This NMF algorithmm is aimed to learn spatially localized, parts-based
+#     subspace representations of visual patterns.
+#     An objective fuction (Kullback-Leibler) localization constrain, in addition
+#     to the non-negativity constrain in the standard NMF.
+#
+#     REF [1]: Learning Spatially Localized, Parts-Based Representations -
+#     S. Z. Li, X. Hou, H. Zhang and Q. Cheng - (2001).
+#
+#     NOTE
+#     ----
+#     Even though the stated objective function has weights alpha and
+#     beta, these disappear in the simplifications made by the authors when
+#     deriving the updates. Therefore, we report the regular Kullback-Leibler
+#     divergence as the error here.
+#     """
+#     def __init__(self, default_max_iter=100):
+#         self.eps = 1e-16
+#
+#     def initializer(self, A, B):
+#         # Normalize columns of matrix A to l1 unity norm
+#         norm_vec = column_norm(A, by_norm='1')
+#         A = A / norm_vec[None, :]
+#
+#         return A, B
+#
+#     def iter_solver(self, Y, A, B, j, it):
+#         # Check the cost function. LNMF only accepts Kullback-Leibler.
+#         if self.cost_function != 'kullback-leibler':
+#             print("Cost function not valid for LNMF algorithm.")
+#             print("Try: 'kullback-leibler'.")
+#             sys.exit()
+#
+#         X = B.T # to keep with the original form
+#         ones = np.ones([Y.shape[0], Y.shape[1]])
+#         AX = A.dot(X) + self.eps # initial reconstruction
+#
+#         # Update A
+#         numerator = A * ((Y / AX).dot(X.T))
+#         denominator = np.maximum(ones.dot(X.T), self.eps)
+#         A = numerator / denominator
+#
+#         # normalize columns of A
+#         norm_vec = column_norm(A, by_norm='1')
+#         A = A / norm_vec[None, :]
+#
+#         # update reconstruction
+#         AX = A.dot(X) + self.eps
+#
+#         # Update B
+#         AX = np.maximum(AX, self.eps)
+#         X = np.sqrt(X * (A.T.dot(Y / AX)))
+#         B = X.T
+#
+#         # Calculate error
+#         Y_hat = A.dot(B.T) + self.eps
+#         error = ((Y * np.log((Y/Y_hat) + self.eps)) - Y + Y_hat).sum(axis=None)
+#
+#         return A, B, error
--- utils/metrics.py
+++ utils/metrics.py
@@ -1,5 +1,24 @@
-import numpy as np
+from numpy import linalg.norm
 
-__all__ = []
+__all__ = ['frobenius_norm']
 
-def kullback_leibler_divergence
+def frobenius_norm(Y, Y_hat):
+    """
+    Calculate the Frobenius Norm between two matrices.
+
+    Parameters
+    ----------
+    Y : numpy.array
+        Original matrix.
+    Y_hat : numpy.array
+        Estimated matrix.
+
+    Returns
+    -------
+    error : float
+        Calculated error using the Frobenius Norm.
+    """
+    diff = Y - Y_hat
+    error = 0.5 * linalg.norm(diff, ord='fro')
+
+    return error
